2e924ec862d2fd858183d52306c44df1f2e0ad68
Gledi Caushaj <gledi.alb@gmail.com>
2022-07-02 14:34:22 +0200

    fix pages?

--- pages/urls.py
+++ pages/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("about", views.about, name="about"),
-    path("privacy-policy", views.privacy_policy, name="privacy_policy"),
-    path("contact-us", views.contact_us, name="contact_us"),
+    path("about/", views.about, name="about"),
+    path("privacy-policy/", views.privacy_policy, name="privacy_policy"),
+    path("contact-us/", views.contact_us, name="contact_us"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,15 +2,16 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "home.html")
+    return render(request, "pages/home.html")
 
 
 def about(request):
-    return render(request, "about.html")
+    return render(request, "pages/about.html")
 
 
 def privacy_policy(request):
-    return render(request, "privacy_policy.html")
+    return render(request, "pages/privacy_policy.html")
+
 
 def contact_us(request):
-    return render(request, "contact_us.html")
+    return render(request, "pages/contact_us.html")
